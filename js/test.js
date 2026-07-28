1492f4b30394b5a2f862cbd920bd2b8e0bd6a159
VENKATA SRINIVASA RAO . GAJULA <venkatasrinivsarao207@gmail.com>
2026-07-28 09:37:36 +0000

    intial commit

---
+++ js/test.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+let student_name = "srinu"
+console.log("student name is:"+student_name)
+let student_age = 21
+let course_name = "python full stack"
+console.log("student age is:"+student_age,"course name is: "+ course_name)
+let fee = 10000
+let discount = 10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("discounted fee is: "+discounted_fee)
+
+let age=18
+if(age>=18){
+    console.log("eligible for admisson")
+}
+else{
+    console.log("not eligible for admission")
+}
